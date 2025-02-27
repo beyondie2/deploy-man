40793c2cf8deaa7f39d0bf63d2cd2e6b2188dd9b
beyondie2 <heewoong7843@gmail.com>
2025-02-27 16:10:27 +0900

    함수 컴포넌트 만듦(Header)

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+const Header =()=> {
+	return (
+		<header>
+			<h1>header</h1>
+		</header>
+	);
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;

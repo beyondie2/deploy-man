caedeb72acaf949fb5380c5317e6c1b8089f3a08
beyondie2 <heewoong7843@gmail.com>
2025-02-27 19:15:18 +0900

    3 Body.js, Footer.js 추가-> 그에 땨른 App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Body from './component/Body';
+import Footer from './component/Footer';
 import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
+      <Footer />
     </div>
   );
 }
